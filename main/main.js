fd9f6e05671159969be0c10de5b10a85d78789ca
MyKatherine <568786833@qq.com>
2018-08-02 14:58:23 +0800

    main.js

--- main/main.js
+++ main/main.js
@@ -1,18 +1,10 @@
-module.exports = function main() {
-    let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
-    console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+// Write your cade below:
+function calRe(a,b) {
+  return a%b
 };
 
-class Sequence {
-  constructor(input) {
-    // Write your code here
-  }
 
-  minimum() {
-    // Write your code here
-  }
 
-  // Write your code here
+module.exports = {
+  calRe
 }
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,12 +1,11 @@
 const main = require('../main/main');
 
 describe('main()', () => {
-    it('should print stats', () => {
-        spyOn(console, 'log');
-        main();
-        expect(console.log).toHaveBeenCalledWith(`o) 最小值 = -2
-o) 最大值 = 92
-o) 元素数量 = 6
-o) 平均值 = 21.83`);
+
+    it('should calculate the remaindar', () => {
+        expect(main.calRe(9, 4)).toBe(1);
+        expect(main.calRe(9, 3)).toBe(0);
     });
+
+
 });
